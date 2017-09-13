4c3794856011b9bdcb1fdf1903ea5449be7a47b8
tkgs0109 <subtkgs@gmail.com>
2017-09-13 19:20:22 +0900

    FileSyncTest

--- fullstack_phase2/02_Sinatra/sinatra-sample/myapp.rb
+++ fullstack_phase2/02_Sinatra/sinatra-sample/myapp.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 get '/' do
   erb :myapp
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
+get '/blogs' do
+  erb :index
+end
+
+get '/a' do
+  erb :a
+end
+
+get '/b' do
+  erb :b
 end
